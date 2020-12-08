2fc2e59e565fc185e5d71984c4fa7a99bf3b03f0
Joseph Chamul <josephchamul@josephs-mbp.attlocal.net>
2020-12-08 06:38:26 -0800

    fixed

--- db.js
+++ db.js
@@ -3,6 +3,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+console.log(process.env.POSTGRES_USER);
 const postgreConnectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`;
 
 console.log(postgreConnectionString);
